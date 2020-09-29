414675ed749eec8bcd44eea2145df5289d3fd500
Khunanon Panjom <khunanon2274@gmail.com>
2020-09-29 17:44:04 +0700

    no message

--- bot.php
+++ bot.php
@@ -28,14 +28,13 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $reply
 			];
-			else{
+			}else{
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
 					'text' => $text
 				];	
 			}	
-			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
