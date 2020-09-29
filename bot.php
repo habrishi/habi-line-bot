20c09706a88d92309b5073f8eef1872be775ef33
Khunanon Panjom <khunanon2274@gmail.com>
2020-09-29 22:27:09 +0700

    Fixed Bugs

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			if($text == 'Meme'){
+			if($text == 'เบื่อจัง'){
 				$messages = [
 					'type' => 'image',
 					'originalContentUrl' => $rand_text,
@@ -116,35 +116,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";	
-		}else if ($event['type'] == 'message' && $event['message']['type'] == 'text'){
-			$ran= $event['message']['text'];
-			$replyToken = $event['replyToken'];
-			if($ran == "เบื่อจัง"){
-			$messages = [
-				'type' => 'images',
-				'previewImageUrl' => $result[array_rand($result)],
-
-			];
 		}
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";	
 	}
 }
 echo "OK";
